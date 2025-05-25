043e1a16bc3023321596c6248c853aa6366ae28a
Azat Yalmaev <azatyalmaev@gmail.com>
2025-05-25 12:34:16 +0300

    add README.md

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Category, Product
+
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'price', 'сategory_item')
+    list_filter = ('сategory_item',)
+    search_fields = ('name','description')
