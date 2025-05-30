54c0b6f89b23d5d2a6f621e0222522aeb715563d
Azat Yalmaev <azatyalmaev@gmail.com>
2025-05-30 10:50:21 +0300

    add README.md

--- catalog/models.py
+++ catalog/models.py
@@ -9,17 +9,22 @@ class Category(models.Model):
         return f'{self.name} {self.description}'
 
     class Meta:
-        pass
+        verbose_name = 'name'
+        verbose_name_plural = 'names'
 class Product(models.Model):
     name = models.CharField(max_length=150, verbose_name='наименование')
     description = models.TextField(max_length=150, verbose_name='описание')
-    сategory_item = models.CharField(max_length=150, verbose_name='категория')
+    сategory_item = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='product')
     price = models.IntegerField(null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+
     def __str__(self):
         return f'{self.name} {self.description}'
 
+
     class Meta:
-        pass
+        verbose_name = 'наименование'
+        verbose_name_plural = 'наименование'
+
