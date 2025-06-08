d99aca378c5db0776b9f5cc9a16ab4c98083c614
Azat Yalmaev <azatyalmaev@gmail.com>
2025-06-08 22:56:15 +0300

    hw3

--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-
+from django.views.generic import ListView, DetailView
 
 from catalog.models import Category, Product
 
@@ -13,13 +13,19 @@ def contacts(requests):
     return render(requests, template_name='catalog/contacts.html')
 
 
-def product_list(requests):
-    products = Product.objects.all()
-    context = {"products": products}
-    return render(requests, template_name='products_list.html', context = context)
+class ProductListView(ListView):
+    model = Product
+    # catalog/product_list.html
 
 
-def product_detail(requests,pk):
-    products = get_object_or_404(Product, pk=pk)
-    context = {"product": products}
-    return render(requests, template_name='product_detail.html', context=context)
+# def product_list(requests):
+#     products = Product.objects.all()
+#     context = {"products": products}
+#     return render(requests, template_name='products_list.html', context = context)
+#
+class ProductDetailView(DetailView):
+    model = Product
+# def product_detail(requests,pk):
+#     products = get_object_or_404(Product, pk=pk)
+#     context = {"product": products}
+#     return render(requests, template_name='product_detail.html', context=context)
